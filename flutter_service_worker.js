eb6ca7ac15d7597491b638f543473b6463587fee
Shoichi Namba <alphabrend@gmail.com>
2020-03-11 09:53:55 +0900

    Create almost

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,12 +4,13 @@ const RESOURCES = {
   "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
+"/assets\hanamaru.png": "4527d00eb813317f01c23e5629d98606",
+"/assets\LICENSE": "0e4098e99d6bbca0717b0b3feb752024",
 "/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "d5dff916c504d68307087222ee9fb525",
-"/main.dart.js": "4474282629a984781176ed771237d271",
+"/main.dart.js": "7aacab25b160b38f3fcd87238d967ac5",
 "/manifest.json": "5352d96966c85d8597a2435a4c6e3572"
 };
 
