6300449e944322688a4246525879d9824045bb50
Shoichi Namba <alphabrend@gmail.com>
2020-03-24 21:57:13 +0900

    Fix bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\hanamaru.png": "4527d00eb813317f01c23e5629d98606",
-"/assets\LICENSE": "0e4098e99d6bbca0717b0b3feb752024",
-"/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "d5dff916c504d68307087222ee9fb525",
-"/main.dart.js": "37fe900a09c0ba68c0ba34776261cffd",
-"/manifest.json": "5352d96966c85d8597a2435a4c6e3572"
+  "/assets/AssetManifest.json": "87c2f38c84c9614d4f25abc94592a76c",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/hanamaru.png": "ba14dd6ebdf7449a61f24b0f8a870578",
+"/assets/LICENSE": "b1296a6510aef6c572b3e2ecf4db09ff",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/web/assets/hanamaru.png": "ba14dd6ebdf7449a61f24b0f8a870578",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/index.html": "4ed2eb37f6c94e27332542bcf6c6cd09",
+"/main.dart.js": "7d22516aad6387ebca2027c816962269",
+"/manifest.json": "1980d3c9c6b072dc2363871c5e9b4a34"
 };
 
 self.addEventListener('activate', function (event) {
@@ -33,9 +34,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
